f1a9ee95f19e0943c81e04e0325807c47d73776b
Viky <50359172+viky1sr@users.noreply.github.com>
2020-04-19 00:21:41 +0700

    fany indexing and bloean indexing

--- Uninitialized_Arrays.py
+++ Uninitialized_Arrays.py
@@ -8,14 +8,72 @@ from numpy import *
 
 # Creating Matrix Arrays
 
-# diagonal = identity(4)
-# print(diagonal)
+diagonal = identity(4)
+print(diagonal)
 
-# x = eye(3, k=1)
-# print(x)
-# y = eye(3, k=-1)
-# print(y)
+x = eye(3, k=1)
+print(x)
+y = eye(3, k=-1)
+print(y)
 
 cons = diag(arange(0, 20, 5)) #mulai dari 0 + 5 > 20
 print(cons) # diagonal 0 + 5 = 5 + 5 = 10 + 5 = 15 > 20 tidak boleh lebih dari 20 , di karenakan 15 + 5 = 20 tidak boleh > 20 bukan >= 20
+print('='*100)
 
+#    CATATAN ( DESKRIPSI)
+
+# a[m] Select element at index m, where m is an integer (start counting form 0).
+# a[-m] Select the n th element from the end of the list, where n is an integer. The last
+# element in the list is addressed as –1, the second to last element as –2, and so on.
+# a[m:n] Select elements with index starting at m and ending at n − 1 (m and n are integers).
+# a[:] or
+# a[0:-1]
+# Select all elements in the given axis.
+# a[:n] Select elements starting with index 0 and going up to index n − 1 (integer).
+# a[m:] or
+# a[m:-1]
+# Select elements starting with index m (integer) and going up to the last element in
+# the array.
+# a[m:n:p] Select elements with index m through n (exclusive), with increment p.
+# a[::-1] Select all the elements, in reverse order
+
+a = arange(0,11) # 0 mulai index , 11 last index , index selalu di muali dari 0
+print(a)
+print(a[1:-1])
+print(a[1:-1:2])
+print(a[:5]) # jika nilai di kasi - (min) maka index yg bernial 5  di print (di karenakan ini n )
+print(a[-5:]) # jika nilai di kasi - (min) maka index yg bernial 5 tidak di print (di karenakan ini m )
+print(a[::-2]) # jika 2 ( p ) adalah - (min) maka di index pertama value na adalah value terakhir (n) jika + (plush) valuna na (m)
+
+print('='*100)
+
+# Multidimensional Arrays
+
+# m = mulai (start) , n = akhir (finish)
+
+f = lambda m, n: n + 10 * m # 10 adalah next colum selalu mulai dari 10++ ( 10 , 20 ,30 dan seterus na )
+A = fromfunction(f , (6,6), dtype=int) # matrix 6x6 ( kolum 6x6 )
+# print(A)
+print(A[:, 1]) # colum pertama yg index na di mulai dari index 1
+print(A[1, :]) # di ambil dari row ke 1 ( row di pyton sama seperti index di mulai dari 0 )
+print(A[:4, :3]) # matrix 4 x 3 di karenakan [:4 , :3]  , di ambil sampai row ke 4 krn :4 ,dan di ambil hanya 3 index saja krn :3
+print(A[4:, 3:]) # di ambil dari row paling akhir
+print(A[::2, ::2]) # di mulai dari 0,0 , row ke 0 + 2 ++ , index ke 0 + 2 ++
+print(A[1::2, 1::3]) # dimuali dari 1,1 . 1::2 di mulai dari row 1 + 2 ++ , 1::3 di muali dari index  1 + 3 = max index sampai 4
+
+# Views
+# Subarrays that are extracted from arrays using slice operations are alternative views of the same underlying array data.
+
+B = A[1:5, 1:5] # 1:5 (m) di mulai dari row ke 1 sampai ke 5 , 1:5 (n) di muali dari index ke 1 dan sampai index ke 5
+print(B)#kamu harus ingat ( index or row ) selalu mulai dari 0
+B[:, :] = 8 # mengisi nilai full pada row pertama dan akhir , dan row selanjutnya bernilai 8 pada index or row 1 sampai sblm terkahir
+print(A)
+
+C = B[1:3, 1:3].copy()
+print(C)
+C[:,:] = 1 # this does not affect B since C is a copy of the view B[1:3, 1:3]
+print(C)
+print(B)
+# In addition to the copy attribute of the ndarray class, an array can also be copied
+# using the function np.copy or, equivalently, using the np.array function with the
+# keyword argument copy=True.
---
+++ fany&bolean_indexing.py
@@ -0,0 +1,89 @@
+from numpy import *
+
+# Fancy Indexing and Boolean-Valued Indexing
+
+A = linspace(0, 1, 11)
+print(A)
+print(A[array([0,2,4])]) # mencari nilai 0 2 4
+print(A[[0,2,4]]) # The same thing can be accomplished by indexing with a Python list
+
+# NumPy array and indexing using a Boolean-valued array
+
+print(A > 0.5)
+print(A[A > 0.5])
+
+# Unlike arrays created by using slices, the arrays returned using fancy indexing and
+# Boolean-valued indexing are not views but rather new independent arrays. Nonetheless,
+# it is possible to assign values to elements selected using fancy indexing:
+
+A = arange(10)
+indices = [2, 4, 6] # indices mengubah nilai pada index [2 4 6]
+B = A[indices]
+B[0] = -1 # tidak mempengaruhin nilai Vairable A
+print(A)
+A[indices] = -1 # ini mengubah nilai Variable A
+print(A)
+
+print('='*100)
+
+# and likewise for Boolean-valued indexing:
+A = arange(10)
+B = A[A > 5]
+B[0] = -1
+print(A)
+A[A > 5] = -1 # jika A (index) > 5 (index) makan nilai index selanjutnya -1
+print(A)
+
+# Reshaping and Resizing
+# Summary of NumPy Functions for Manipulating the Dimensions and the Shape of Arrays
+"""
+Function/Method         ||  Description
+np.reshape              ||  Reshape an N-dimensional array. The total number of elements must  
+np.ndarray.reshape      ||  remain the same.
+np.ndarray.flatten      ||  Creates a copy of an N-dimensional array, and reinterpret it as a one-dimensional array (i.e., all dimensions are collapsed into one). 
+np.ravel                ||  Create a view (if possible, otherwise a copy) of an N-dimensional array 
+np.ndarray.ravel        ||  in which it is interpreted as a one-dimensional array
+np.squeeze              ||  Removes axes with length 1.
+np.expand_dims,         ||  Add a new axis (dimension) of length 1 to an array, where np
+np.newaxis              ||  newaxis is used with array indexing.
+np.transpose            ||  Transpose the array. The transpose operation corresponds to reversing (or more generally, permuting) the axes of the array.
+np.ndarray.transpose    ||  
+np.ndarray.T            ||
+np.hstack               ||  Stacks a list of arrays horizontally (along axis 1): for example, given a list of column vectors, appends the columns to form a matrix.
+np.vstack               ||  Stacks a list of arrays vertically (along axis 0): for example, given a list of row vectors, appends the rows to form a matrix.
+np.dstack               ||  Stacks arrays depth-wise (along axis 2).
+np.concatenate          ||  Creates a new array by appending arrays after each other, along a given axis.
+np.resize               ||  Resizes an array. Creates a new copy of the original array, with the
+                            requested size. If necessary, the original array will be repeated to fill
+                            up the new array.
+np.append               ||  Appends an element to an array. Creates a new copy of the array
+np.insert               ||  Inserts a new element at a given position. Creates a new copy of the array
+np.delete               ||  Deletes an element at a given position. Creates a new copy of the array.
+"""
+
+# Reshaping
+# data = array([[1,2] , [3,4]]) or bisa menggunakan matrix('1,2; 3,4')
+data = matrix('1,2; 3,4')
+print(data)
+print(reshape(data, (1,4)))
+print(data.reshape(4))
+
+data = array([[1, 2], [3, 4]])
+print(data.flatten())
+print(data.flatten().shape)
+
+data = arange(0,5)
+colum = data[:, newaxis]
+print(colum)
+row = data[newaxis, :]
+print(row)
+
+print('='*100)
+
+data = arange(5)
+print(data)
+print(vstack((data,data,data))) # vstack = vertically
+print(hstack((data,data,data))) # hstack = horizontally
+
+data = data[:, newaxis]
+print(hstack((data, data, data)))
